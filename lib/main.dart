84b5dbebbd4a1fd28bb535c9bb58c99e7de59b17
Rakesh <rakeshudaan012@gmail.com>
2026-05-06 13:11:22 +0530

    Initial commit: Flutter Chat Bot App setup

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:chat_bot/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget{
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
